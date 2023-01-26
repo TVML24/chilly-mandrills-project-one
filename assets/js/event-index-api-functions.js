c8c6a76a4a75711906349b5bcffdd4165a39cd3a
Tim <tvanmookletcher@gmail.com>
2023-01-26 12:59:55 +1030

    added integration of inputs into eventindexpage, tested and working. Some small bugfixes

--- assets/js/event-index-api-functions.js
+++ assets/js/event-index-api-functions.js
@@ -1,5 +1,12 @@
 var cardBox = document.getElementById("card-box");
 var searchBtn = document.getElementById("search-button");
+var artistInputEl = document.getElementById("artist-name");
+var locationinputEl = document.getElementById("location-dropdown");
+
+// spotify developer credentials
+
+var client_id = "a43d54d7a7d344feb4f19b7be7c700c1";
+var client_secret = "c018f8890f564297ae26c852bb7ee4cd";
 
 // this button calls the clear cards function
 // after inputs are implemented we can add the search functions from the landing page API to this API where necessary
@@ -196,7 +203,82 @@ function clearCards () {
     while (cardBox.firstChild) {
        cardBox.removeChild(cardBox.lastChild);
     }
+    formSubmitHandler();
 }
 
+// These run the search function and are identical to the functions in the landing page script
+function formSubmitHandler() {
+  
+  // the below line replaces any space with a hyphen
+  var artist = artistInputEl.value.replace(" ", "-").trim();
+      console.log(artist);
+  var location = locationinputEl.options[locationinputEl.selectedIndex].value;
+  // this line combines the two variables, because we can only effectively pass one variable to the next function
+  var searchVariables = artist + " " + location;
+  // var artist = artistInputEl.value.trim();
+  if (searchVariables) {
+    getToken(searchVariables);
+  }
+};
 
+function getToken(searchVariables) {
+    var token = "";
+    fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: {
+        Authorization: "Basic " + btoa(`${client_id}:${client_secret}`),
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+      },
+      body: "grant_type=client_credentials",
+    })
+      .then(function (response) {
+        if (!response.ok) {
+          throw response.json();
+        }
+        return response.json();
+      })
+      .then(function (Results) {
+        console.log(Results);
+  // this line adds the token to the front of the variable that we now pass to the next function
+        var token = Results.access_token + " " + searchVariables;
+        console.log(token);
+        getGenre(token);
+      });
+  }
 
+  function getGenre(token) {
+    // url will be updated with dynamic element, this is to test that genre is logging to the console in the interim
+    // this line splits the big old passed variable into 3 at every space
+    var holderArray = token.split(" ");
+    console.log(holderArray);
+    // these variables are made from the split pieces of the array created by splitting the big old variable 
+    var token = holderArray[0];
+    var artist = holderArray[1];
+    var location = holderArray[2];
+    var apiUrl =
+      "https://api.spotify.com/v1/search?q=" +
+      artist +
+      "&type=artist&limit=1";
+    fetch(apiUrl, {
+      headers: { Authorization: "Bearer " + token },
+    })
+      .then(function (response) {
+        if (!response.ok) {
+          throw response.json();
+        }
+        return response.json();
+      })
+      .then(function (Results) {
+        // this grabs the genre from the spotify API
+      var genre = Results.artists.items[0].genres;
+      // this generates the new URL
+      var queryString = "./eventindex.html?events?apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0&locale=*&marketId=" + location + "&countryCode=AU&classificationName=" + genre;
+        console.log(queryString);
+      // this passes the URL to the next function q: don't know why I couldn't do it in this function, but it didn't like it.
+        documentAssign(queryString);
+      });
+  }  
+
+  function documentAssign (queryString) {
+    location.assign(queryString);
+  }
